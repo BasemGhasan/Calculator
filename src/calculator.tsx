cb81b624e2962dc2119198a69d4b8a45b123c6e9
BasemGhasan <BasemGhasan@gmail.com>
2025-12-07 20:39:58 +0800

    Clean the repo & build the initial structure of the project

---
+++ src/calculator.tsx
@@ -0,0 +1,19 @@
+//import { useState } from 'react'
+import Button from './components/button'
+import './app.css'
+
+function Calculator() {
+
+  return (
+    <div className='root'>
+      <div className='calculator-body'>
+        <h2>Calculator</h2>
+        <Button>
+          =
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
---
+++ src/components/button.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  children?: React.ReactNode;
+}
+
+function Button({ children }: Props) {
+  return <button>{children}</button>;
+}
+
+export default Button;
